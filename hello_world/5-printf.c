f9e1af50e5b728073ba04605323abd4384e39782
taralexandra <taralexandra@gmail.com>
2024-06-18 10:57:29 +0200

    ajout fichier 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - fonction principale
+ * suite
+ * suite
+ * bla
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
+
