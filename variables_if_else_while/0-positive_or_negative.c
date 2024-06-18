39c7fca826079a3b3a2616a27d581f6ed644f0ab
taralexandra <taralexandra@gmail.com>
2024-06-18 16:44:16 +0200

    ajout commentaire fichier 0 avec main et return

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,22 +1,24 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main -  headers goes there
+ *
+ * Return: 0;
+ */
 int main(void)
 {
-        int n;
+	int n;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        /* your code goes there */
-        if (n > 0)
-          printf("%d is positive\n", n);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	printf("%d is positive\n", n);
 	else if (n == 0)
-          printf("%d is zero\n", n);
-        else
-          printf("%d is negative\n", n);
+	printf("%d is zero\n", n);
+	else
+	printf("%d is negative\n", n);
 
-        return (0);
+	return (0);
 }
