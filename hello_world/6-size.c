e232d6f937a4819703b76d6875e317209311834d
taralexandra <taralexandra@gmail.com>
2024-06-18 11:32:35 +0200

    ajout fichier 6

---
+++ hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - fonction principale
+ * suite
+ * suite
+ * bla
+ * Return: 0
+ */
+int main(void)
+{
+  printf("Size of a char: \n", );
+  printf("Size of an int: \n", );
+  printf("Size of a long int: \n", );
+  printf("Size of a long long  int: \n", );
+  printf("Size of a float: \n", );
+return (0);
+}
