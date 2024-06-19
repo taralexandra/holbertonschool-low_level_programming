1165143ead135fc8757d3141643ecea0b0d481aa
taralexandra <taralexandra@gmail.com>
2024-06-19 10:41:00 +0200

    ajout fichier 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - afficher l'alphabet
+  *
+  * Return: 0;
+  */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+	putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
