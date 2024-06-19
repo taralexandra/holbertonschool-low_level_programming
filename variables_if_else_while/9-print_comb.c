852c668b83ff3d643455b39bb55237c9c3e65bb1
taralexandra <taralexandra@gmail.com>
2024-06-19 14:46:29 +0200

    MAJ espacement fichier 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -12,11 +12,11 @@ int main(void)
 	for (c = '0'; c <= '9'; c++)
 	{
 		putchar(c);
-	if (c != '9')
-	{
-		putchar(',');
-		putchar(' ');
-	}	
+		if (c != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 
