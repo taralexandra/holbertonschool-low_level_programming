5c24467b0359b721a2b8c56fcc6884ab2e1bdc40
taralexandra <taralexandra@gmail.com>
2024-06-19 11:17:26 +0200

    ajout fichier 5 - afficher les chiffres de 0 à 10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - afficher les chiffres de 0 compris à 10 compris
+  *
+  * Return: 0;
+  */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 10; n++)
+	{
+	putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
