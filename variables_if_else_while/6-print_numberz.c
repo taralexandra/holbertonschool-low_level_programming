3ab4f454812feea372989f801401e8c698158a8e
taralexandra <taralexandra@gmail.com>
2024-06-19 13:39:00 +0200

    ajout fichier 6 - afficher les chiffres dits c de 0 à 9

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main - afficher les chiffres de 0 compris à 9 compris
+  *
+  * Return: 0;
+  */
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c < '9'; c++)
+	
+	putchar(c);
+	
+	putchar('\n');
+	return (0);
+}
+
