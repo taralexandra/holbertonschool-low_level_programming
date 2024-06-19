7784582f6c6271e80b9ed9bd1f74eca86e3c7143
taralexandra <taralexandra@gmail.com>
2024-06-19 13:48:43 +0200

    ajout fichier 7 - reverse alphabet

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  * main - afficher l'alphabet
+  *
+  * Return: 0;
+  */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch <= 'a'; ch++)
+	{
+	putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
+
