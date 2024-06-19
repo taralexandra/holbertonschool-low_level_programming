fea2e611998ca8ede6834087757a6ece26bbe2f0
taralexandra <taralexandra@gmail.com>
2024-06-19 14:37:49 +0200

    ajout fichier 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+  * main - afficher tous les single digit (chiffres) de 0 à 9
+  * séparés par un espace et une virgule ex : 0, 1, 2,...
+  * Return: 0;
+  */
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	if (c != '9')
+	{
+		putchar(',');
+		putchar(' ');
+	}	
+	}
+	putchar('\n');
+
+	return (0);
+}
+
