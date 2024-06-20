32a9b40b5b3cb8c77e5abe1f889ea1732ac1c689
taralexandra <taralexandra@gmail.com>
2024-06-20 11:17:58 +0200

    ajout fichier 0 - dossier Projet 3

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * main - afficher _putchar
+  * puis une nouvelle ligne
+  *
+  * Return: 0;
+  */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Fichier README du dossier functions nested loops pour le projet 3.
