1969123f419ebff24e47220ae0a84b191f2e79de
taralexandra <taralexandra@gmail.com>
2024-06-21 14:29:48 +0200

    ajout fichier 5

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_sign - check the code.
+ * @n: description.
+ * Return: Always 0.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+		return (0);
+}
