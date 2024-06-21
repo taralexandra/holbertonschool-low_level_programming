c444052c78bdb4ca0c5bd389810132fc9c73d1ff
taralexandra <taralexandra@gmail.com>
2024-06-21 11:10:02 +0200

    ajout modifs et ajout fichier main.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,16 +1,8 @@
 #include "main.h"
 /**
- * main - check the code in order to print the alphabet
+ * print_alphabet - check the code in order to print the alphabet
  *
  * Return: Always 0.
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-/**
- * print_alphabet - Prints the alphabet in lowercase followed by a new line
  */
 void print_alphabet(void)
 {
