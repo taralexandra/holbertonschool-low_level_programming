5e906680e485e104787a09974694b8f2ef6b3559
taralexandra <taralexandra@gmail.com>
2024-06-21 14:08:13 +0200

    ajout fichier 4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - check the code.
+ * @c: The character to print
+ * Return: Always 0.
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
