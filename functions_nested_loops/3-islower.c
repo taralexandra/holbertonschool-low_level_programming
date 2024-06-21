5ba8a8d716305890863fc7c7da9a13b7e1c14f66
taralexandra <taralexandra@gmail.com>
2024-06-21 13:50:17 +0200

    ajout fichier 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - check the code.
+ * @c: The character to print
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c >= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
