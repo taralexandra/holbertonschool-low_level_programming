9f76fa415f4d00fe9af140f1f224ae1b44590b4c
taralexandra <taralexandra@gmail.com>
2024-06-22 08:23:36 +0200

    ajout fichier 10 calcul

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - check the code
+ * @a: premier chiffre.
+ * @b: second chiffre.
+ * c déja déf dans file putchar de base,  resultat de l'opé.
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
