290d61541580b44de17b49a855822015a18806be
taralexandra <taralexandra@gmail.com>
2024-06-24 10:45:19 +0200

    ajout fichiers étape 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - check the code.
+ * @a: multiplicande.
+ * @b: multiplicateur.
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = b * a;
+	return (c);
+}
