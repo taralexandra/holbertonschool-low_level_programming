071578927063e51314a7d4ca23909f3a1b225cec
taralexandra <taralexandra@gmail.com>
2024-06-24 16:32:26 +0200

    ajout fichiers pour étape 10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -9,14 +9,16 @@
 
 void print_triangle(int size)
 {
-	int i, j;
+	int i, j, k;
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j <= i; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
-		_putchar('#');
+		_putchar(' ');
 		}
+		for (k = size - i - 1; k < size; k++)
+			_putchar('#');
 		_putchar('\n');
 	}
 }
