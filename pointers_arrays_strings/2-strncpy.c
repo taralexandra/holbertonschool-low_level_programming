f970e3253642766deeceaf6ea4baee794e677358
taralexandra <taralexandra@gmail.com>
2024-06-28 16:55:49 +0200

    modifs fichier .c étape 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -11,14 +11,15 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
+
 	i = 0;
-//Ici, les caract st copiés jusqu'à arriver au car nul ou que i atteigne n
+
+
 	while (src[i] != '\0' && i < n)
 	{
 		dest[i] = src[i];
 		i++;
 	}
-//Si on a pas encore atteint n caractères alors on ajoute des caractères nuls. 
 	while (i < n)
 	{
 		dest[i] = '\0';
