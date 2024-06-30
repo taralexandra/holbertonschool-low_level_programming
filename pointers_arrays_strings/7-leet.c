ab79f48cbcccd6d6a0c10416c77b76603b5c3af0
taralexandra <taralexandra@gmail.com>
2024-06-30 15:27:43 +0200

    ajout fichiers étape 7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *leet - check the code for
+ * @s: chaîne à encoder.
+ * Return: Always 0.
+ */
+char *leet(char *s)
+{
+	int i, j;
+
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+			}
+		}
+	}
+	return (s);
+}
