126427f99be3cf84b7bcd90103f6cc4eeb38f7b6
taralexandra <taralexandra@gmail.com>
2024-07-01 11:32:37 +0200

    ajout fichiers étape 2 - more arrays and pointers 01 07

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * *_strchr - fonction qui permet de chercher un char.
+  * @s: chaîne où chercher.
+  * @c: caractère à rechercher.
+  * The most wanted.
+  * Return: caractère ou nul.
+  */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (NULL);
+}
