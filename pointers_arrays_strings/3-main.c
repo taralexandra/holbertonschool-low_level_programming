203b9715696625f803fe72165390fb3e17cb75ae
taralexandra <taralexandra@gmail.com>
2024-07-01 14:35:13 +0200

    ajout fichiers étape 3 - more arrays and pointers - 01 07

--- pointers_arrays_strings/3-main.c
+++ pointers_arrays_strings/3-main.c
@@ -1,18 +1,18 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
-    char s1[] = "Hello";
-    char s2[] = "World!";
+    char *s = "hello, world";
+    char *f = "oleh";
+    unsigned int n;
 
-    printf("%d\n", _strcmp(s1, s2));
-    printf("%d\n", _strcmp(s2, s1));
-    printf("%d\n", _strcmp(s1, s1));
+    n = _strspn(s, f);
+    printf("%u\n", n);
     return (0);
 }
