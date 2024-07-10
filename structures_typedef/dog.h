f4bb03332acb32a6741c3f3d13a7eda4d53b601d
taralexandra <taralexandra@gmail.com>
2024-07-10 11:16:08 +0200

    modifs fichier dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -14,14 +14,14 @@ struct dog
 	char *owner;
 };
 
+ /**
+ 22  * dog_t - Typedef for struct dog
+ 23  */
+ 24 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog * d);
-
-/**
- * dog_t - Typedef for struct dog
- */
-typedef struct dog dog_t;
 
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
