7ad7a515043deac304dc768146864259fd5bffd0
taralexandra <taralexandra@gmail.com>
2024-07-10 13:46:24 +0200

    remplacement fichier 3 test checker

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,50 +1,48 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
-  * **alloc_grid - f retourne ptr vers tab 2D.
-  * @height: hauteur du tab 2D.
-  * @width: largeur du tab 2D.
-  * Return: width, height ou NULL en cas d'échec.
-  */
+ * alloc_grid - Returns a pointer to a 2-dimensional array of
+ *               integers with each element initalized to 0.
+ * @width: The width of the 2-dimensional array.
+ * @height: The height of the 2-dimensional array.
+ *
+ * Return: If width <= 0, height <= 0, or the function fails - NULL.
+ *         Otherwise - a pointer to the 2-dimensional array of integers.
+ */
 int **alloc_grid(int width, int height)
 {
-	int **tab2D;
-	int h_index, w_index;
+	int **twoD;
+	int hgt_index, wid_index;
 
 	if (width <= 0 || height <= 0)
-	{
 		return (NULL);
-	}
 
-	tab2D = malloc(sizeof(int *) * height);
+	twoD = malloc(sizeof(int *) * height);
 
-	if (tab2D == NULL)
-	{
+	if (twoD == NULL)
 		return (NULL);
-	}
-	for (h_index = 0; h_index < height; h_index++)
-	{
-		tab2D[h_index] = malloc(sizeof(int) * width);
 
-		if (tab2D[h_index] == NULL)
+	for (hgt_index = 0; hgt_index < height; hgt_index++)
+	{
+		twoD[hgt_index] = malloc(sizeof(int) * width);
+
+		if (twoD[hgt_index] == NULL)
 		{
-			for (; h_index >= 0; h_index--)
-			{
-				free(tab2D[h_index]);
+			for (; hgt_index >= 0; hgt_index--)
+				free(twoD[hgt_index]);
 
-				free(tab2D);
-				return (NULL);
-			}
+			free(twoD);
+			return (NULL);
 		}
 	}
-	for (h_index = 0; h_index < height; h_index++)
+
+	for (hgt_index = 0; hgt_index < height; hgt_index++)
 	{
-		for (w_index = 0; w_index < width; w_index++)
-		{
-			tab2D[h_index][w_index] = 0;
-		}
+		for (wid_index = 0; wid_index < width; wid_index++)
+			twoD[hgt_index][wid_index] = 0;
 	}
-	return (tab2D);
+
+	return (twoD);
 }
