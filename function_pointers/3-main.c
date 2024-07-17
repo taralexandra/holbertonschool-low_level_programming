b64eec7dcc510e6e6ad4c1df14754b7489cf3a26
taralexandra <taralexandra@gmail.com>
2024-07-17 15:21:52 +0200

    ajout modifs fichiers 3 main.c projet calculatrice - fonction pointers

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 
 	operation = get_op_func(argv[2]);
 
-	if (operation == NULL)
+	if (operation == NULL || (argv[2][1] != '\0'))
 	{
 		printf("Error\n");
 		exit(99);
