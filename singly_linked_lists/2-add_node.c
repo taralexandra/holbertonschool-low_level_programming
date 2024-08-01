12fecf20aaf9ad059c1cdc8eb2a93d4062f85ddf
taralexandra <taralexandra@gmail.com>
2024-08-01 10:11:59 +0200

    I am changing some elements to my code for task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -14,25 +14,20 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	int lenght = 0;
 
 	new_node = malloc(sizeof(list_t));
-
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
 
+	while (str[lenght])
+		lenght++;
+
+	new_node->len = lenght;
 	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-
-	new_node->len = strlen(str);
-
 	new_node->next = *head;
-
 	*head = new_node;
 
 	return (new_node);
