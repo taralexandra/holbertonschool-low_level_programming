27aff6fa2f5cc99e136dcaae9b9a92277298f99c
taralexandra <taralexandra@gmail.com>
2024-08-01 13:38:24 +0200

    I am updation the header file main.h

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -1,10 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Essential libraries */
 
+#include <stdio.h>
+#include <stdlib.h>
+
+/* _putchar prototype */
+
+int _putchar(char c);
 
 /* Prototypes of functions */
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 
 
 
